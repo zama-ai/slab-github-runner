710890392224fd544f9e6fed77169577ed7ab4ec
Luis Montero <17201527+fd0r@users.noreply.github.com>
2024-02-02 14:34:29 +0100

    fix(typo): fix typo in timeout message

--- src/gh.js
+++ src/gh.js
@@ -55,7 +55,7 @@ async function waitForRunnerRegistered(label) {
   }
 
   core.error(
-    `A timeout of ${timeoutSeconds} minutes is exceeded. Your AWS EC2 instance was not able to register itself in GitHub as a new self-hosted runner.`
+    `A timeout of ${timeoutSeconds} seconds is exceeded. Your AWS EC2 instance was not able to register itself in GitHub as a new self-hosted runner.`
   )
   throw new Error('GitHub self-hosted runner registration error')
 }
