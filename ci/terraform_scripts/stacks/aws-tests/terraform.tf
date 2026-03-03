984d5b866828fad4e4d4253d235348cb2cb1ba4a
David Testé <david.teste@zama.ai>
2026-03-03 11:41:49 +0100

    chore(ci): add terraform aws test cases
    This ensure AWS backend via Terraform is supported in the action.

---
+++ ci/terraform_scripts/stacks/aws-tests/terraform.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.34"
+    }
+  }
+}
+provider "aws" {
+  region = "eu-west-3"
+}
+
+variable "instance_type" {
+  type        = string
+  description = "EC2 instance type to be used"
+}
+
+variable "instance_label" {
+  type        = string
+  description = "EC2 instance name to display in console"
+}
+
+variable "user_data" {
+  type = string
+  description = "Script that will be run at instance startup"
+}
+
+resource "aws_instance" "cpu_small" {
+  ami = "ami-0eda00173fe323828"
+  instance_type = var.instance_type
+  user_data = var.user_data
+
+  tags = {
+    Name = var.instance_label
+  }
+}
+
+output "instance_id" {
+  value       = aws_instance.cpu_small.id
+  description = "Unique ID of the EC2 instance"
+}
