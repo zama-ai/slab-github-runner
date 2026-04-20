5c35f7150bebbc32e2f3a2016c92bc3067864a40
David Testé <david.teste@zama.ai>
2026-04-20 18:07:16 +0200

    chore(ci): add terraform test cases
    This ensure AWS and Scaleway providers via Terraform are supported in
    the action.

---
+++ ci/terraform_scripts/stacks/aws-tests/terraform.tf
@@ -0,0 +1,57 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.41"
+    }
+  }
+  required_version = "~> 1.14"
+}
+provider "aws" {
+  region = "eu-west-3"
+}
+
+# Provided via ci/slab.toml
+variable "instance_type" {
+  type        = string
+  description = "EC2 instance type to be used"
+}
+
+# Provided by Slab server
+variable "instance_label" {
+  type        = string
+  description = "EC2 instance name to display in console"
+}
+
+# Provided by Slab server
+variable "user_data" {
+  type        = string
+  description = "Script that will be run at instance startup"
+}
+
+resource "aws_instance" "cpu_small" {
+  ami           = "ami-0eda00173fe323828"
+  instance_type = var.instance_type
+  user_data     = var.user_data
+  monitoring    = true
+
+  # checkov:skip=CKV2_AWS_41:No rule is used since the spawned instance will do nothing
+
+  ebs_optimized = true
+  root_block_device {
+    encrypted = true
+  }
+  metadata_options {
+    http_endpoint = "enabled"
+    http_tokens   = "required"
+  }
+
+  tags = {
+    Name = var.instance_label
+  }
+}
+
+output "instance_id" {
+  value       = aws_instance.cpu_small.id
+  description = "Unique ID of the EC2 instance"
+}
---
+++ ci/terraform_scripts/stacks/scaleway-tests/terraform.tf
@@ -0,0 +1,66 @@
+terraform {
+  required_providers {
+    scaleway = {
+      source  = "scaleway/scaleway"
+      version = "~> 2.73"
+    }
+  }
+  required_version = "~> 1.14"
+}
+provider "scaleway" {
+  zone = "fr-par-2"
+}
+
+# Provided via Slab server env
+variable "project_id" {
+  type        = string
+  description = "Scaleway project ID to attached to"
+}
+
+# Provided via ci/slab.toml
+variable "instance_type" {
+  type        = string
+  description = "Scaleway instance type to be used"
+}
+
+# Provided by Slab server
+variable "instance_label" {
+  type        = string
+  description = "Instance name to display in console"
+}
+
+# Provided by Slab server
+variable "user_data" {
+  type        = string
+  description = "Script that will be run at instance startup"
+}
+
+resource "scaleway_instance_ip" "github_runner" {
+  project_id = var.project_id
+}
+
+resource "scaleway_instance_security_group" "github_runner" {
+  project_id              = var.project_id
+  inbound_default_policy  = "drop"
+  outbound_default_policy = "accept"
+}
+
+resource "scaleway_instance_server" "cpu_small" {
+  project_id = var.project_id
+  image      = "ubuntu_noble"
+  type       = var.instance_type
+  name       = var.instance_label
+
+  ip_id = scaleway_instance_ip.github_runner.id
+
+  user_data = {
+    "cloud-init" = var.user_data
+  }
+
+  security_group_id = scaleway_instance_security_group.github_runner.id
+}
+
+output "instance_id" {
+  value       = scaleway_instance_server.cpu_small.id
+  description = "Unique ID of the Scaleway instance"
+}
