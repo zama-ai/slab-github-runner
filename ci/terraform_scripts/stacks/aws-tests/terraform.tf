52b79bc6df8f338b421bfd28710f0b7efb8cbe55
David Testé <david.teste@zama.ai>
2026-04-20 18:07:16 +0200

    chore(ci): add terraform test cases
    This ensure AWS and Scaleway providers via Terraform are supported in
    the action.

---
+++ ci/terraform_scripts/stacks/aws-tests/terraform.tf
@@ -0,0 +1,58 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.41"
+    }
+  }
+  required_version = ">= 0.13"
+}
+provider "aws" {
+  region = "eu-west-3"
+}
+
+variable "instance_type" {
+  type        = string
+  description = "EC2 instance type to be used"
+}
+
+variable "instance_label" {
+  type        = string
+  description = "EC2 instance name to display in console"
+}
+
+variable "user_data" {
+  type        = string
+  description = "Script that will be run at instance startup"
+}
+
+resource "aws_instance" "cpu_small" {
+  ami                  = "ami-0eda00173fe323828"
+  instance_type        = var.instance_type
+  user_data            = var.user_data
+  monitoring           = true
+  iam_instance_profile = "CIBotEC2Role"
+
+  root_block_device {
+    encrypted = true
+  }
+
+  ebs_optimized = true
+  ebs_block_device {
+    device_name = "/dev/sdg"
+    encrypted   = true
+  }
+
+  metadata_options {
+    http_tokens = "required"
+  }
+
+  tags = {
+    Name = var.instance_label
+  }
+}
+
+output "instance_id" {
+  value       = aws_instance.cpu_small.id
+  description = "Unique ID of the EC2 instance"
+}
